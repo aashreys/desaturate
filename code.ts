24f4b84d2b4f44fcfa1051bd20d6ddb212e41cb4
Aashrey Sharma <aashreys@Aashreys-MacBook-Pro.local>
2021-05-15 18:16:59 -0400

    Improved performance by skipping nodes that do not need desaturation and reducing fill cloning

--- code.ts
+++ code.ts
@@ -1,7 +1,7 @@
 // Desaturate is a simple plugin that desaturates image fills in Figma. 
 // Select any Frame or Vector object with an image fill and run the plugin.
 
-const IS_LOGGING_ENABLED = false;
+const IS_LOGGING_ENABLED = true;
 
 const ALLOWED_NODE_TYPES = [
   "COMPONENT",
@@ -18,30 +18,33 @@ const ALLOWED_NODE_TYPES = [
   "VECTOR"
 ];
 
-const NOTIFICATION_TIME = 2500;
+const NOTIFICATION_TIME = 3000;
 
 var numDesaturatedNodes = 0;
 
+
+
+// MAIN
 try {
   if (figma.currentPage.selection.length > 0) {
-    _log(figma.currentPage.selection);
-    for(const node of figma.currentPage.selection) {
-      desaturateNodeTree(node);
-    }
+    for(const node of figma.currentPage.selection) desaturateNodeTree(node);
     notifyStatus(numDesaturatedNodes);
   } else {
-    notifyUI("Nothing selected. Select something to desaturate.");
+    notifyUI("Nothing selected. Select something with an image fill to desaturate.");
   }
 } catch (error) {
   _logError(error);
 }
 figma.closePlugin();
 
+
+
+// FUNCTIONS
 function notifyStatus(numDesaturatedNodes) {
   if (numDesaturatedNodes > 0) {
     notifyUI("Desaturated " + numDesaturatedNodes + " image " + (numDesaturatedNodes > 1 ? "fills" : "fill") + ".");
   } else {
-    notifyUI("No image fills detected. Please check selection.")
+    notifyUI("Nothing to desaturate. Please check selection.")
   }
 }
 
@@ -50,47 +53,56 @@ function notifyUI(message: string) {
 }
 
 function desaturateNodeTree(node) {
-  _log("Processing node tree: ");
-  _log(node);
-  _desaturateNode(node); // Desaturate current level
+  if (isNodeAllowed(node) && isNodeSaturated(node)) {
+    _desaturateNode(node); // desaturate current level
+  } else {
+    _log("Skipping node: " + node.type);
+  }
+  
   if ("children" in node) {
     for (const child of node.children) {
-      desaturateNodeTree(child); // Desaturate next level
+      desaturateNodeTree(child); // desaturate next level
     }
-  }
-}
-
-function _desaturateNode(node) {
-  if (isNodeAllowed(node)) {
-    _log("Desaturating node: ");
-    _log(node);
-    if ("fills" in node) {
-      const fills = _clone(node.fills);
-      if (Array.isArray(fills)) {
-        for (const fill of fills) {
-          _desaturateFill(fill);
-        }
-      } else {
-        _desaturateFill(fills);
-      }
-      node.fills = fills;
-    }
-  } else {
-    _log("Skipping node because it is disallowed: " + node.type);
-  }
-}
-
-function _desaturateFill(fill) {
-  _log("Desaturating fill: ");
-  _log(fill);
-  if ("type" in fill && fill.type === "IMAGE") {
-    fill.filters.saturation = -1;
-    numDesaturatedNodes++;
   }
 }
 
 function isNodeAllowed(node) {
   return ALLOWED_NODE_TYPES.includes(node.type);
+}
+
+function isNodeSaturated(node) {
+  if ("fills" in node) {
+    const fills = Array.isArray(node.fills) ? node.fills : [node.fills];
+    for (const fill of fills) {
+      if (fill.type === "IMAGE" && fill.filters.saturation > -1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function isFillSaturated(fill) {
+  return fill.type === "IMAGE" && fill.filters.saturation > -1;
+}
+
+function _desaturateNode(node) {
+  _log("Desaturating node:");
+  _log(node);
+  const fills = Array.isArray(node.fills) ? _clone(node.fills) : [_clone(node.fills)];
+  for (const fill of fills) {
+    if (isFillSaturated(fill)) {
+      _desaturateFill(fill);
+      numDesaturatedNodes++;
+    }
+  }
+  node.fills = fills;
+}
+
+function _desaturateFill(fill) {
+  _log("Desaturating fill:");
+  _log(fill);
+  fill.filters.saturation = -1;
 }
 
 function _log(object) {
